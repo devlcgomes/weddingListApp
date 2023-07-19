9750a104d6ec09d71fdb12e79bc9ea3286a07a39
devlcgomes <dev.lucianogomes@gmail.com>
2023-07-19 17:41:18 -0300

    Creating guest class getters and setters to access data.

--- app/src/main/java/devandroid/gomes/weddinglist/model/Guest.java
+++ app/src/main/java/devandroid/gomes/weddinglist/model/Guest.java
@@ -3,8 +3,49 @@ package devandroid.gomes.weddinglist.model;
 // Template class to create new guests
 public class Guest {
 
+    // Atributes
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String contact;
+
     // Constructor....
     public Guest() {
 
     }
+
+    // Geters and setters
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+
+
 }
--- app/src/main/java/devandroid/gomes/weddinglist/view/MainActivity.java
+++ app/src/main/java/devandroid/gomes/weddinglist/view/MainActivity.java
@@ -17,8 +17,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Create a new Guest
+        // Create a new Guest and set content for atributes
         guest = new Guest();
+        guest.setFirstName("Luciano");
+        guest.setLastName("Gomes");
+        guest.setEmail("gomes.dev@gmail.com");
+        guest.setContact("64992273262");
+
     }
 
 }
